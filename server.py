e59e1d23370eadc21fbb15bbfb35f109c963303f
Rizky Ilhamsyah <54507408+superizky27@users.noreply.github.com>
2020-03-15 14:59:08 +0700

    Update server.py
    Dictionary

--- server.py
+++ server.py
@@ -5,3 +5,17 @@ import threading
 # Batasi hanya pada path /RPC2 saja supaya tidak bisa mengakses path lainnya
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
+
+with SimpleXMLRPCServer(("127.0.0.1", 8080),
+                    requestHandler=RequestHandler) as server:
+	server.register_introspection_functions()
+
+	filmtayang = {
+
+	}
+
+	film1 = {
+		'Judul' : 'Judul',
+		'Harga' : 'Rp. 35.000',
+		'Sisa tiket' : 20,
+	}
