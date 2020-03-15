5726eb817bcef8a7ff4dbf84bd10200b3d44db8e
Rizky Ilhamsyah <54507408+superizky27@users.noreply.github.com>
2020-03-15 16:05:58 +0700

    update client server

--- client.py
+++ client.py
@@ -2,6 +2,19 @@ import xmlrpc.client
 
 
 s = xmlrpc.client.ServerProxy('http://127.0.0.1:8080')
+
+print("1 Daftar Film dan Jam Tayang")
+print("2 Pesan Tiket Bioskop")
+print("3 Harga Tiket")
+print("4 Sisa Tiket yang Tersedia")
+
+print("==========")
+print("5 Admin")
+print("Ubah jam Tayang")
+print("Ubah Harga Tiket")
+
+print(s.show())
+
 '''
 buat tampilan untuk daftar film yang sedang tayang, 
 pemesanan tiket bioskop, 
--- server.py
+++ server.py
@@ -11,11 +11,74 @@ with SimpleXMLRPCServer(("127.0.0.1", 8080),
 	server.register_introspection_functions()
 
 	filmtayang = {
+		"film1" : {
+	  	'Judul' : 'Parasite',
+		'Harga' : 'Rp. 40.000',
+		'Sisa' : 10,
+		'Studio' : 1,
+		'Jam' : '12:30'
+		},
 
+		"film2" : {
+		'Judul' : 'Avengers: End Game',
+	  	'Harga' : 'Rp. 40.000',
+	  	'Sisa' : 34,
+	  	'Studio' : 2,
+	  	'Jam' : '14:30'
+	 	},
+
+	 	"film3" : {
+	  	'Judul' : 'Weathering with You',
+	  	'Harga' : 'Rp. 40.000',
+	  	'Sisa' : 25,
+	  	'Studio' : 1,
+	  	'Jam' : '14:00'
+	 	},
+
+	 	"film4" : {
+	  	'Judul' : 'Code Geass: Fukatsu no Lelouch',
+	  	'Harga' : 'Rp. 40.000',
+	  	'Sisa' : 22,
+	  	'Studio' : 2,
+	  	'Jam' : '13:30'
+	 	},
+
+	 	"film5" : {
+	  	'Judul' : 'Parasite',
+	  	'Harga' : 'Rp. 40.000',
+	  	'Sisa' : 8,
+	  	'Studio' : 1,
+	  	'Jam' : '19:00'
+	 	},
+
+	 	"film6" : {
+	  	'Judul' : 'Parasite',
+	  	'Harga' : 'Rp. 40.000',
+	  	'Sisa' : 5,
+	  	'Studio' : 2,
+	  	'Jam' : '20:30'
+	 	}
 	}
 
-	film1 = {
-		'Judul' : 'Judul',
-		'Harga' : 'Rp. 35.000',
-		'Sisa tiket' : 20,
-	}
+	lock = threading.Lock()
+
+	def tampilfilm():
+		temp = []
+		lock.acquire()
+		for x,y in filmtayang.items():
+			print("========================")
+			print("Judul Film: ", y["Judul"])
+			print("Harga Tiket: ", y["Harga"])
+			print("Sisa Tiket", y["Sisa"])
+			print("Studio", y["Studio"])
+			print("Jam Tayang", y["Jam"])
+			print("========================")
+
+			temp.append(y)
+		lock.release()
+		return y
+	server.register_function(tampilfilm, 'show')
+
+	print ("Server Bioskop berjalan...")
+	#Run the server's main loop
+	server.serve_forever()
